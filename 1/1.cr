53eb1e79cdfa3aa9f04198bf3ca3819cfa5fc4a3
Joel Kuhn <joelakuhn@gmail.com>
2022-12-01 10:04:09 -0500

    Cleaned up parsing with compact_map

--- 1/1.cr
+++ 1/1.cr
@@ -1,8 +1,6 @@
 elves = File.open("input.txt").gets_to_end.split(/\r?\n\r?\n/).map do |e|
-    e.split(/\r?\n/)
-    .select{|i| i != ""}
-    .map(&.to_i)
+    e.split(/\r?\n/).compact_map(&.to_i?)
 end
 
 puts elves.map{|e| e.sum}.max
-puts elves.map{|e| e.sum}.sort.reverse[0..2].sum
+puts elves.map{|e| e.sum}.sort.last(3).sum
