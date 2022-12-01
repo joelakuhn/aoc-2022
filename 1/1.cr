7a4c30c8c3df135bcc42ed7411ff1ff682e8cb77
Joel Kuhn <joelakuhn@gmail.com>
2022-12-01 10:05:40 -0500

    Switched to short blocks for sums

--- 1/1.cr
+++ 1/1.cr
@@ -2,5 +2,5 @@ elves = File.open("input.txt").gets_to_end.split(/\r?\n\r?\n/).map do |e|
     e.split(/\r?\n/).compact_map(&.to_i?)
 end
 
-puts elves.map{|e| e.sum}.max
-puts elves.map{|e| e.sum}.sort.last(3).sum
+puts elves.map(&.sum).max
+puts elves.map(&.sum).sort.last(3).sum
